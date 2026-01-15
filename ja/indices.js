cc5293b860633ad202787db533a62bc69a0249bd
Nick Fabek <nickfabek@gmail.com>
2026-01-14 22:32:31 -0500

    asdf

---
+++ ja/indices.js
@@ -0,0 +1,32 @@
+// "TAG": [INDICES]
+const data_japanese_words_by_tag = {
+"four_elements": [0],
+}
+
+// "ORTHOGRAPHY": [INDICES]
+const data_japanese_words_by_orthography = {
+"水":[0],
+"山":[1],
+"食べる":[2],
+"行く":[3],
+"大きい":[4],
+"小さい":[5],
+"空":[6],
+"走る":[7],
+"新しい":[8],
+"友達":[9],
+}
+
+// "PRONUNCIATION": [INDICES]
+const data_japanese_words_by_pronunciation = {
+"みず":[0],
+"やま":[1],
+"たべる":[2],
+"いく":[3],
+"おおきい":[4],
+"ちいさい":[5],
+"そら":[6],
+"はしる":[7],
+"あたらしい":[8],
+"ともだち":[9],
+}
---
+++ ja/words.js
@@ -0,0 +1,13 @@
+// "WORD | KANA | ROMAJI | CLASS | MEANING | TAGS"
+const data_japanese_words = [
+"水|みず|mizu|noun|water|four_elements,beverage,liquid,substance",
+"山|やま|yama|noun|mountain|place,geography",
+"食べる|たべる|taberu|verb|to eat|food,action",
+"行く|いく|iku|verb|to go|movement,action",
+"大きい|おおきい|ookii|i-adjective|big|size,description",
+"小さい|ちいさい|chiisai|i-adjective|small|size,description",
+"空|そら|sora|noun|sky|nature,place,weather",
+"走る|はしる|hashiru|verb|to run|movement,action",
+"新しい|あたらしい|atarashii|i-adjective|new|description,time",
+"友達|ともだち|tomodachi|noun|friend|people,relationship"
+];
